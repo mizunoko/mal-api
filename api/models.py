a82d0300267c4c93678a05d58860517d650c58b4
mr_robot <agentgamer2004@gmail.com>
2021-09-05 19:38:49 +0530

    api works boi!

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Token(models.Model):
-	token=models.CharField(max_length=30)
+	token=models.CharField(max_length=64)
 	issued_to=models.CharField(max_length=120)
 	issued_on=models.DateField(auto_now_add=True)
 	def __str__(self):
