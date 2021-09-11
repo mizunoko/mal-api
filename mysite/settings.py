90fdd430bbe7e2075f111b7bbf99e79da85dfa5b
mr_robot <agentgamer2004@gmail.com>
2021-09-11 13:04:27 +0530

    second commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
