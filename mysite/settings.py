d3f2e0bc5fb228c0173c5ce56259cc8370b28ddc
mr_robot <agentgamer2004@gmail.com>
2021-09-11 18:12:26 +0530

    heroku I am coming!

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-j8aij8yynb^s$jb^)nh3axts$$82$c%9gwrnt+wgnrdxgl===f
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', '*.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'mal-api-one.herokuapp.com', 'mal-api-two.herokuapp.com']
 
 
 # Application definition
