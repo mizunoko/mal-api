a79569e9d8231cdc2ded4e69d17c9b7f4bc8ecd7
mr_robot <agentgamer2004@gmail.com>
2021-09-11 12:47:49 +0530

    first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-j8aij8yynb^s$jb^)nh3axts$$82$c%9gwrnt+wgnrdxgl===f
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.0']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.0', 'api-testing-1337.herokuapp.com']
 
 
 # Application definition
